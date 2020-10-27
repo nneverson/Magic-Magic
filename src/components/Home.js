a8dd167afaa47aa61b47bab13635b04fd0d0a8e5
nneverson <neelyneverson@yahoo.com>
2020-10-27 14:30:21 -0400

    components added

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+    const [ anime, setAnime ] = useState([]);
+    
+    
+    useEffect(() => {
+       
+        
+    }, [])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return (
+        <div>
+            <h1>this is the home comeponent</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className='logo'>
+            <div>Magic Magic</div>
+            <ul className='navelink'>
+                <li><a href='#'>Home</a> </li>
+                <li><a href='#'>Search</a> </li> 
+                <li><a href='#'>About</a> </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Nav;
