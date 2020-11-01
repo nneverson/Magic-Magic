bce9a8d75302789eb48e1c31fac304aec4a8a9a2
nneverson <neelyneverson@yahoo.com>
2020-11-01 18:11:37 -0500

    added overflow to details css

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
 	return (
 		<div>
 			<div className='aboutText'>
-			<h1>About this app</h1>
+			<h1>About This App</h1>
 				<p>This app was created to be a resource for those who want to get a taste
 				of something different when it comes to magical girl animes. The shows in this data base have adult themes such as graphic violence, and psycholocially disturbing events. For age 17 +</p>
 			</div>
